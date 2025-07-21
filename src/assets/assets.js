67fe25a9c2c6a2efdcfd17834d6569848aeb120b
Prisca Masereli <priscamasereli@gmail.com>
2025-07-21 18:15:33 +0200

    feat:(assets)-Add general assets for the project

---
+++ src/assets/assets.js
@@ -0,0 +1,36 @@
+import { youtube } from "./youtube.png";
+import { user } from "./user.png";
+import { sent } from "./sent.png";
+import { question } from "./question.png";
+import { plus } from "./plus.png";
+import { compass } from "./compass.png";
+import { gallery } from "./gallery.png";
+import { mic } from "./mic.png";
+import { gear } from "./gear.png";
+import { gemini } from "./gemini.png";
+import { picture } from "./picture.png";
+import { message } from "./message.png";
+import { menu } from "./menu.png";
+import { history } from "./history.png";
+import { bulb } from "./bulb.png";
+import { code } from "./code.png";
+import { use } from "react";
+
+export const assets = {
+  bulb,
+  code,
+  compass,
+  gallery,
+  gear,
+  gemini,
+  history,
+  menu,
+  message,
+  mic,
+  picture,
+  plus,
+  question,
+  sent,
+  user,
+  youtube,
+};
