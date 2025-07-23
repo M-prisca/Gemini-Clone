36285c361800ea1bb863859927c9305872eb4caf
Prisca Masereli <priscamasereli@gmail.com>
2025-07-23 09:10:42 +0200

    feat(component-sidebar): Add hide functionality to the top sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,41 +1,47 @@
-import React from 'react'
-import "./Sidebar.css"
-import { assets } from '../../assets/assets'
+import React, { useState } from "react";
+import "./Sidebar.css";
+import { assets } from "../../assets/assets";
 
 const Sidebar = () => {
-  return (
-    <div className='sidebar'>
-        <div className='top'>
-            <div><img className='menu' src={assets.menu} alt="" /></div>
-            <div className='new-chat'>
-              <img src={assets.plus} alt="" />
-              <p>New Chat</p>
-            </div>
-            <div className="recent">
-              <p className="recent-title">Recent</p>
-              <div className="recent-entry">
-                <img src={assets.message} alt="" />
-                <p>What is react ...</p>
-              </div>
-            </div>
-        </div>
-        <div className='bottom'>
-          <div className="bottom-item recent-entry">
-            <img src={assets.question} alt="" />
-            <p>Help</p>
-          </div>
-          <div className="bottom-item recent-entry">
-            <img src={assets.history} alt="" />
-            <p>Activity</p>
-          </div>
-          <div className="bottom-item recent-entry">
-            <img src={assets.gear} alt="" />
-            <p>Settings</p>
-          </div>
-        </div>
-      
-    </div>
-  )
-}
+  const [extended, setExtended] = useState(false);
 
-export default Sidebar
+  return (
+    <div className="sidebar">
+      <div className="top">
+        <div>
+          <img className="menu" src={assets.menu} alt="" />
+        </div>
+        <div className="new-chat">
+          <img src={assets.plus} alt="" />
+          {extended ? <p>New Chat</p> : null}
+        </div>
+
+        {extended ? (
+          <div className="recent">
+            <p className="recent-title">Recent</p>
+            <div className="recent-entry">
+              <img src={assets.message} alt="" />
+              <p>What is react ...</p>
+            </div>
+          </div>
+        ) : null}
+      </div>
+      <div className="bottom">
+        <div className="bottom-item recent-entry">
+          <img src={assets.question} alt="" />
+          <p>Help</p>
+        </div>
+        <div className="bottom-item recent-entry">
+          <img src={assets.history} alt="" />
+          <p>Activity</p>
+        </div>
+        <div className="bottom-item recent-entry">
+          <img src={assets.gear} alt="" />
+          <p>Settings</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
