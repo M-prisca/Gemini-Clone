e3960e719e18f407d11cbe286f2e7313ffd527e4
Prisca Masereli <priscamasereli@gmail.com>
2025-07-23 08:19:27 +0200

    Feat(Component Sidebar): Add top and bottom contens

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,20 +1,19 @@
-import { youtube } from "./youtube.png";
-import { user } from "./user.png";
-import { sent } from "./sent.png";
-import { question } from "./question.png";
-import { plus } from "./plus.png";
-import { compass } from "./compass.png";
-import { gallery } from "./gallery.png";
-import { mic } from "./mic.png";
-import { gear } from "./gear.png";
-import { gemini } from "./gemini.png";
-import { picture } from "./picture.png";
-import { message } from "./message.png";
-import { menu } from "./menu.png";
-import { history } from "./history.png";
-import { bulb } from "./bulb.png";
-import { code } from "./code.png";
-import { use } from "react";
+import youtube from "./youtube.png";
+import user from "./user.jpg";
+import sent from "./sent.png";
+import question from "./question.png";
+import plus from "./plus.png";
+import compass from "./compass.png";
+import gallery from "./gallery.png";
+import mic from "./mic.png";
+import gear from "./gear.png";
+import gemini from "./gemini.jpeg";
+import picture from "./picture.png";
+import message from "./message.png";
+import menu from "./menu.png";
+import history from "./history.png";
+import bulb from "./bulb.png";
+import code from "./code.png";
 
 export const assets = {
   bulb,
