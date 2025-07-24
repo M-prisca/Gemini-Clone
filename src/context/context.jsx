f6aa2a41d9bc5d74cac410d55a00b9db2c4d3c45
Prisca Masereli <priscamasereli@gmail.com>
2025-07-25 00:24:13 +0200

    fix(context,main):fix react rendering issue

--- src/context/context.jsx
+++ src/context/context.jsx
@@ -5,8 +5,9 @@ export const Context = createContext();
 const ContextProvider = (props) => {
   const contextValue = {};
 
-  return;
-  <Context.Provider value={contextValue}>{props.children}</Context.Provider>;
+  return (
+    <Context.Provider value={contextValue}>{props.children}</Context.Provider>
+  );
 };
 
 export default ContextProvider;
